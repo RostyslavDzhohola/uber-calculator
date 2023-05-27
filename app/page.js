21b7766bf83d4a24d17dace034456975cddb8b52
Rostyslav <dg1h1t@protonmail.com>
2023-05-27 15:46:57 -0500

    adding some text to landing page

--- app/page.js
+++ app/page.js
@@ -9,6 +9,9 @@ export default function Home() {
   return (
     <div>
       <main className={styles.main}>
+        Hello uber drviers. I want to help you to calculate your actual hopurly earnings. You can go to the calculator page and enter your data.
+        For the future launch of the app, you will be able to sign up and save your data.
+        Subsribe to get notified when the app is ready.
         {/* <h1 className="text-5xl bg-indigo-400 p-3 rounded-2xl">Home page</h1> */}
         <SignUp />
       </main>
