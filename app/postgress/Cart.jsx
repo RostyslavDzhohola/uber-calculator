0d6c6da0e619e8167f5f63902afd9cd94585d504
Rostyslav <dg1h1t@protonmail.com>
2023-06-04 17:11:03 -0500

    Update SQL query to fetch driver's cart

--- app/postgress/Cart.jsx
+++ app/postgress/Cart.jsx
@@ -1,7 +1,7 @@
 import { sql } from "@vercel/postgres";
 
 export default async function Cart({ params }) {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+  const { rows } = await sql`SELECT * from DRIVERS where user_id=${params.driver_id}`;
 
   return (
     <div>
